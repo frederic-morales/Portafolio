d45f05af0b831d5bb934c3813e61907ebd3ba603
Frederick Morales pc1 <tocomorales30@gmail.com>
2023-01-30 17:40:21 -0600

    Casi terminada el header de nuestro proyecto - siuuu

--- code.js
+++ code.js
@@ -1,7 +1,11 @@
 const asideMobileMenu = document.querySelector(".header-aside-mobile");
 const hamburgerIcon = document.querySelector("#hamburger-icon");
+const xIcon = document.querySelector("#x-icon");
+const body = document.querySelector("#body");
 
 hamburgerIcon.addEventListener("click", mobileMenu);
+xIcon.addEventListener("click", mobileMenu);
+body.addEventListener("click", closedMenu)
 
 function mobileMenu(){
     const isMobileMenuOpen = asideMobileMenu.classList.contains("active");
